d835458a461c04c5d9f4411544ad98360434011f
Junichi MARUYAMA <junichi.maruyama.rg@riken.jp>
2023-09-12 19:58:38 +0900

    Fixed FigS1/KEGG_Pathway_Enrichment/preproc.sh

--- Okamoto2021/FigS1/KEGG_Pathway_Enrichment/preproc.sh
+++ Okamoto2021/FigS1/KEGG_Pathway_Enrichment/preproc.sh
@@ -8,11 +8,11 @@ theSpecies=$1
 
 DATA_DIR=./Data
 
-curl "http://rest.kegg.jp/link/${theSpecies}/pathway" > ${DATA_DIR}/path.${theSpecies}.xrefs
+curl "https://rest.kegg.jp/link/${theSpecies}/pathway" > ${DATA_DIR}/path.${theSpecies}.xrefs
 
-curl "http://rest.kegg.jp/list/pathway/${theSpecies}" | awk -F" - " '{ print $1 }' > ${DATA_DIR}/${theSpecies}_pathway_list.txt
+curl "https://rest.kegg.jp/list/pathway/${theSpecies}" | awk -F" - " '{ print $1 }' | awk -F"\t" '{ print "path:"$1"\t"$2 }' > ${DATA_DIR}/${theSpecies}_pathway_list.txt
 
-curl "http://rest.kegg.jp/list/${theSpecies}" > ${DATA_DIR}/${theSpecies}_gene_list.txt
+curl "https://rest.kegg.jp/list/${theSpecies}" > ${DATA_DIR}/${theSpecies}_gene_list.txt
 
 grep -v "^path:${theSpecies}05\|${theSpecies}04930\|${theSpecies}04931\|${theSpecies}04940" ${DATA_DIR}/${theSpecies}_pathway_list.txt > ${DATA_DIR}/${theSpecies}_non_disease_pathway_list.txt
 
